29f87b055e636024610f9508e2d333fddbcc649f
Garry201199 <72191123+Garry201199@users.noreply.github.com>
2023-03-18 23:57:31 +0530

    Defect- not able to scroll due to new scroll pkg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Converter from "./features/currency/Converter"
-import Scroll from './features/currency/comp/Scroll'
 const App = () =>  {
 
   return (
     <div className="flex font-monte justify-center items-center text-black bg-gradient-to-br from-[#200122] to-[#6f0000] w-full min-h-screen ">
        <Converter/>
-       <Scroll />
     </div>
   )
 }
